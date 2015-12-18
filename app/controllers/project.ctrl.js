c6da8b34863549b20c653645b66df2a762e69888
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-12-19 02:54:21 +0500

    error handling for promises

--- app/controllers/project.ctrl.js
+++ app/controllers/project.ctrl.js
@@ -1,18 +1,40 @@
 senhoApp.controller('ProjectCtrl', function ($log, $scope, apiService, $stateParams, $q) {
 	$log.log('Hello from your Controller: ProjectCtrl in module main:. This is your controller:', this);
 	var id = $stateParams.projectId;
-	$scope.$parent.promise.then(function () {
-		$scope.promise = apiService.getProject(id);
-		$scope.promise.then(function (payload) {
-			$scope.project = payload.data;
-			console.log($scope.project);
+	if ($scope.$parent.promise) {
+		$scope.$parent.promise.then(function () {
+			$scope.promise = apiService.getProject(id);
+			$scope.promise.then(function (payload) {
+				$scope.project = payload.data;
+				console.log($scope.project);
+			});
 		});
-	});
-	$scope.$parent.imagesPromise.then(function () {
-		$scope.imagePromise = apiService.getProjectImages(id);
-		$scope.imagePromise.then(function (payload) {
-			$scope.images = payload.data;
-			console.log('scope images: ' + $scope.images.length);
-		})
-	})
+	} else {
+		$scope.$parent.promise = apiService.getProjects();
+		$scope.$parent.promise.then(function () {
+			$scope.promise = apiService.getProject(id);
+			$scope.promise.then(function (payload) {
+				$scope.project = payload.data;
+				console.log($scope.project);
+			});
+		});
+	}
+	if ($scope.$parent.imagesPromise) {
+		$scope.$parent.imagesPromise.then(function () {
+			$scope.imagePromise = apiService.getProjectImages(id);
+			$scope.imagePromise.then(function (payload) {
+				$scope.images = payload.data;
+				console.log('scope images: ' + $scope.images.length);
+			})
+		});
+	} else {
+		$scope.$parent.imagesPromise = apiService.getImages();
+		$scope.$parent.imagesPromise.then(function () {
+			$scope.imagePromise = apiService.getProjectImages(id);
+			$scope.imagePromise.then(function (payload) {
+				$scope.images = payload.data;
+				console.log('scope images: ' + $scope.images.length);
+			})
+		});
+	}
 });
