75681befe551dc6aead47b5bb807e5881da3e2a4
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-12-19 15:23:13 +0500

    changed sweetalert directory

--- app/controllers/home.ctrl.js
+++ app/controllers/home.ctrl.js
@@ -10,7 +10,7 @@ senhoApp.controller('HomeCtrl', function ($log, $scope, apiService) {
 		console.log('sending message "' + $scope.email.message + '"" from ' + $scope.email.email);
 		if ($scope.email.email === '' || $scope.email.message === '') {
 			$scope.validation = 'Please fill in your email and your message.';
-			swal('', 'Please fill in your email and message.', 'info');
+			sweetAlert('', 'Please fill in your email and message.', 'info');
 			console.log($scope.validation);
 		} else {
 			$scope.validation = '';
@@ -18,11 +18,11 @@ senhoApp.controller('HomeCtrl', function ($log, $scope, apiService) {
 			$scope.promise = apiService.sendEmail($scope.email);
 			$scope.promise.then(function (data) {
 				if (data.data === "1") {
-					swal('Thank you!', 'We\'ll get back to you soon.', 'success');
+					sweetAlert('Thank you!', 'We\'ll get back to you soon.', 'success');
 					$scope.email.email = '';
 					$scope.email.message = '';
 				} else {
-					swal('Sorry...', 'Something went wrong with the server. Please try again.', 'error');
+					sweetAlert('Sorry...', 'Something went wrong with the server. Please try again.', 'error');
 				}
 				console.log(data.data);
 				$scope.sending = false;
